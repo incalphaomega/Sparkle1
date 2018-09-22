b3190737b78f93d223b8b9cf1850fb81ce86ea42
incalphaomega <43068161+incalphaomega@users.noreply.github.com>
2018-09-22 18:06:50 +0300

    добавил TextEditor

---
+++ Sparkle/Handlers/TextEditor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparkle.Handlers
+{
+    class TextEditor
+    {
+        //переключение между экземплярами TextInput
+        //редактирование строки StringBuilder активного экземпляра TextInput 
+        //логика для удаления Backspace, Ctrl+Backspace
+        //каретку и её анимацию
+        //выделение текста
+        //вставку и копирование из буфера обмена
+    }
+}
