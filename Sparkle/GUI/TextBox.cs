24c9f8c548af2fc365b33e0333337e8a65260fbb
incalphaomega <43068161+incalphaomega@users.noreply.github.com>
2018-09-24 18:49:16 +0300

    temp TextBox

--- Sparkle/GUI/TextBox.cs
+++ Sparkle/GUI/TextBox.cs
@@ -14,10 +14,15 @@ namespace Sparkle.GUI
         Vector2 position;
         Rectangle textRect;
 
+        Texture2D textCursor;
+        Vector2 textCursorPos;
+
         TextInput input;
         TextEditor edit;
 
         StringBuilder text;
+
+        bool isActive;
 
         public TextBox(Vector2 position)
         {
@@ -25,12 +30,16 @@ namespace Sparkle.GUI
 
             input = new TextInput();
             edit = new TextEditor();
+
+            isActive = false;
         }
 
         public void Update()
         {
             input.Update(text);
             edit.Update(text);
+
+            textCursorPos = new Vector2(position.X + Main.consoleFont.MeasureString(text).Y, position.Y);
         }
 
         public void Draw(SpriteBatch s)
