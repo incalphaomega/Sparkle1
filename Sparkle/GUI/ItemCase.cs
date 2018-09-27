239802510a7fa4f019b574a7383a550d8282acc2
RinaKoner <43066788+RinaKoner@users.noreply.github.com>
2018-09-27 19:31:26 +0300

    Положила начало Инвентарю.

---
+++ Sparkle/GUI/ItemCase.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sparkle.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparkle.GUI
+{
+    class ItemCase
+    {
+       static Texture2D texture;
+        Vector2 position;
+        Rectangle rect;
+
+        string item;
+        string info;
+        
+
+        public ItemCase(Vector2 position)
+        {
+            this.position = position;
+            item = "Пустая ячейка";
+        }
+       public static void LoadContent()
+        {
+       //     Handlers.ContentLoader.Load<Texture2D>("Texture/GUI/ItemCase");
+        }
+
+        public void Update()
+        {
+            rect = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
+            showInfo();
+          
+
+        }
+
+        public void Draw(SpriteBatch s)
+        {
+            s.DrawString(Main.consoleFont, info, position, Color.Black);
+        }
+
+        public void putItemIn()
+        {
+
+        }
+        public void replaceItame()
+        {
+
+        }
+        //public string getItame() {}
+        public void replaceItem()
+        {
+
+        }
+        public void showInfo()
+        {
+            if (rect.Contains(Input.mousePoint))
+            {
+                info = item;   
+            }
+            else
+            {
+                info = "";
+            }
+        }
+    }
+}
